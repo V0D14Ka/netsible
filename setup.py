8b2552dc70526ef1013f209ee7963a696f3aa7fd
Вадим Яшин <minecraft35510880@gmail.com>
2024-06-08 01:00:56 +1000

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 # setup.py
+import pathlib
 
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+here = pathlib.Path(__file__).parent.resolve()
+
 setup(
     name='netsible',
     version='0.1',
     packages=find_packages(),
+    package_dir={'': ''},
     entry_points={
         'console_scripts': [
             'netsible=cli.main:cli',
