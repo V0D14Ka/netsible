bb672f1e67a861d058a1802a180e22c1ab9597c8
Вадим Яшин <minecraft35510880@gmail.com>
2024-06-08 00:49:55 +1000

    setup

---
+++ cli/main.py
@@ -0,0 +1,46 @@
+import os
+
+from dotenv import load_dotenv
+from netmiko import ConnectHandler
+
+
+def ssh_connect_and_execute(device_type, hostname, port, user, password, command, keyfile=None):
+    try:
+        device = {
+            'device_type': device_type,
+            'host': hostname,
+            'port': port,
+            'username': user,
+            'password': password,
+        }
+
+        connection = ConnectHandler(**device)
+        out = connection.send_command(command)
+        connection.disconnect()
+
+        return out
+    except Exception as e:
+        raise e
+
+
+def test():
+    load_dotenv()
+
+    # Параметры подключения
+    device_type = os.getenv('device_type')
+    hostname = os.getenv('hostname')
+    port = os.getenv('port')
+    user = os.getenv('user')
+    password = os.getenv('password')
+    command = os.getenv('command')
+
+    print(device_type, hostname, port, user, password, command)
+    output = ssh_connect_and_execute(device_type, hostname, port, user, command)
+
+    if output:
+        print("Результат команды:")
+        print(output)
+
+
+if __name__ == "__main__":
+    print("Hello world!")
---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='netsible',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'netsible=cli.main:cli',
+        ],
+    },
+    install_requires=requirements,
+)
