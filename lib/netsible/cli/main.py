ad4ba7e48b55241e1805d87589b45b85fb5236b3
Вадим Яшин <minecraft35510880@gmail.com>
2024-06-08 20:47:48 +1000

    ping task

--- lib/netsible/cli/main.py
+++ lib/netsible/cli/main.py
@@ -1,12 +1,16 @@
 import argparse
 import os
+import ping3
+import pexpect
+import subprocess
+import platform
 
 from dotenv import load_dotenv
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler, NetmikoTimeoutException
 from netsible.cli.config import version
 
 
-def ssh_connect_and_execute(device_type, hostname, port, user, password, command, keyfile=None):
+def ssh_connect_and_execute(device_type, hostname, user, password, command, keyfile=None, port=22):
     try:
         device = {
             'device_type': device_type,
@@ -25,35 +29,66 @@ def ssh_connect_and_execute(device_type, hostname, port, user, password, command
         raise e
 
 
-def test():
-    load_dotenv()
+def find_client_info(client_name, file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            client_info = dict(part.split('=') for part in line.strip().split(' '))
+            if client_info.get('name') == client_name:
+                return client_info
+    return None
 
-    # Параметры подключения
-    device_type = os.getenv('device_type')
-    hostname = os.getenv('hostname')
-    port = os.getenv('port')
-    user = os.getenv('user')
-    password = os.getenv('password')
-    command = os.getenv('command')
 
-    print(device_type, hostname, port, user, password, command)
-    output = ssh_connect_and_execute(device_type, hostname, port, user, command)
+def ping_ip(client_info):
+    result = ping3.ping(client_info['host'])
 
-    if output:
-        print("Результат команды:")
-        print(output)
+    if result is False or result is None:
+        print("Ping failed.")
+        return
+
+    print(f"Ping successful. Round-trip time: {result} ms")
+
+
+def task(client_info):
+    command = 'uptime'
+    try:
+        output = ssh_connect_and_execute(device_type=client_info['type'], hostname=client_info['host'],
+                                         user=client_info['user'], password=client_info['pass'], command=command)
+
+        if output:
+            print("Результат команды:")
+            print(output)
+
+    except NetmikoTimeoutException as e:
+        print("Связь с устройством отсутствует")
 
 
 def main():
     parser = argparse.ArgumentParser(description='Netsible Command Line Tool')
+    parser.add_argument('-v', '--version', action='version', version=version)
 
-    parser.add_argument('--version', action='version', version=version)
+    parser.add_argument('-m', '--method', choices=['test', 'task'], help='choose the method', default='task')
+    parser.add_argument('-f', '--force', action='store_true', help='force operation')
 
-    parser.add_argument('host', type=str, help='Domain name/name from hosts/ip address')
-    parser.add_argument('type', type=str, help='Inventory OS type')
-    parser.add_argument('task', type=str, help='Task to do')
+    parser.add_argument('host', type=str, help='target host name from hosts.txt')
+    parser.add_argument('task', type=str, help='task from to execute on the target host')
+
     args = parser.parse_args()
+
+    try:
+        file_path = r'C:\pass.secret' if platform.system() == 'Windows' else '/etc/netsible/hosts.txt'
+        client_info = find_client_info(args.host, file_path)
+
+        if args.method == 'test':
+            ping_ip(client_info)
+        else:
+            task(client_info)
+
+    except FileNotFoundError as e:
+        print(f"Отсутствует файл {file_path}, убедитесь в том, что вы его создали")
+        return
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    ping_ip('192.168.31.10')
+    # test('client1')
