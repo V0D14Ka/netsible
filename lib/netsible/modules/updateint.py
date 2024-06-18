14d00ce103b7d1fce7bf1ed276d9c558469e87e8
Вадим Яшин <minecraft35510880@gmail.com>
2024-06-18 15:49:37 +1000

    add router os template

--- lib/netsible/modules/updateint.py
+++ lib/netsible/modules/updateint.py
@@ -64,9 +64,89 @@ interface {{ interface_name }}
  no shutdown
 {% endif %}
 
+{% if write %}
+ do write
+{% else %}
+
 {% if description is not defined and ip_address is not defined and mtu is not defined and speed is not defined and duplex is not defined and bandwidth is not defined and encapsulation is not defined and authentication is not defined and switchport_mode is not defined and shutdown is not defined %}
  no shutdown
 {% endif %}'''
+
+template_router_os = '''
+/interface {{ interface_name }}
+{% if description is defined %}
+ description="{{ description }}"
+{% endif %}
+
+{% if ip_address is defined and subnet_mask is defined %}
+ ip address={{ ip_address }}/{{ subnet_mask }}
+{% endif %}
+
+{% if secondary_ip_addresses is defined %}
+{% for ip in secondary_ip_addresses %}
+ ip address={{ ip.address }}/{{ ip.subnet }} add
+{% endfor %}
+{% endif %}
+
+{% if mtu is defined %}
+ mtu={{ mtu }}
+{% endif %}
+
+{% if speed is defined %}
+ speed={{ speed }}
+{% endif %}
+
+{% if duplex is defined %}
+ duplex={{ duplex }}
+{% endif %}
+
+{% if bandwidth is defined %}
+ tx-rate={{ bandwidth }}
+{% endif %}
+
+{% if encapsulation is defined %}
+ ; Encapsulation is not directly configurable on MikroTik
+{% endif %}
+
+{% if authentication is defined %}
+ ; Authentication settings (like MD5) may vary, configure as needed
+{% endif %}
+
+{% if switchport_mode is defined %}
+ /interface ethernet switch port
+ {% if switchport_mode == 'access' and access_vlan is defined %}
+ set {{ interface_name }} vlan-mode=secure vlan-header=add \
+    default-vlan-id={{ access_vlan }}
+ {% endif %}
+ {% if switchport_mode == 'trunk' and trunk_allowed_vlans is defined %}
+ set {{ interface_name }} vlan-mode=secure vlan-header=add \
+    allowed-vlan={{ trunk_allowed_vlans }}
+ {% endif %}
+ {% if switchport_mode == 'trunk' and native_vlan is defined %}
+ set {{ interface_name }} vlan-mode=secure vlan-header=add \
+    native-vlan-id={{ native_vlan }}
+ {% endif %}
+{% endif %}
+
+{% if shutdown %}
+ disabled=yes
+{% else %}
+ disabled=no
+{% endif %}
+
+{% if write %}
+ /export
+{% else %}
+{% if description is not defined and ip_address is not defined and mtu is not defined and speed is not defined and duplex is not defined and bandwidth is not defined and encapsulation is not defined and authentication is not defined and switchport_mode is not defined and shutdown is not defined %}
+ enabled=yes
+{% endif %}
+{% endif %}
+'''
+
+module_template = {
+    'cisco': template_cisco,
+    'routeros': template_router_os
+}
 
 
 class UpdateInt(BasicModule):
@@ -102,7 +182,7 @@ class UpdateInt(BasicModule):
                 'verbose': True,  # включить вывод подробной информации о подключении
             }
 
-            template = Template(template_cisco)
+            template = Template(module_template.get(self.module))
             output = template.render(self.params)
             commands = [line for line in output.splitlines() if line.strip()]
 
